a56470237160e245f473487a08c1cc8119caff71
LordHenry90 <notaro.enrico@gmail.com>
2024-10-15 16:59:27 +0200

    clear

--- app.py
+++ app.py
@@ -108,8 +108,12 @@ def download_zip():
 @app.route('/clear', methods=['POST'])
 def clear_files():
     for folder in [UPLOAD_FOLDER, DATA_FOLDER, IMAGES_FOLDER]:
-        shutil.rmtree(folder)
-        os.makedirs(folder)
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
